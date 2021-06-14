188fb0bcc81067e706f6f1164e6d7e4776117903
B. Klara Simon <borbala.klara@gmail.com>
2021-06-14 22:33:58 +0200

    add selector

---
+++ script.js
@@ -0,0 +1,31 @@
+const hourElm = document.querySelector(".hour");
+const minElm = document.querySelector(".minute");
+const secondElm = document.querySelector(".second");
+const timeElm = document.querySelector(".time");
+const dateElm = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const days = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
